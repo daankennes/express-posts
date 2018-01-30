2455f82ca4ab9398e405d9f1d58f8f69a1df5025
daank <32707088+daankennes@users.noreply.github.com>
2018-01-30 14:34:35 +0100

    Volledig werkend

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const request = require('request');
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Listening on port 3000');
+app.listen(process.env.PORT || 4000, () => {
+  console.log('Listening on port 4000');
 });
 
 app.set('view engine', 'ejs');
@@ -17,7 +18,14 @@ app.get('/', (req, res) => {
 });
 
 // List all posts
-app.get('/posts', (req, res) => {});
+app.get('/posts', (req, res) => {
+  request('http://jsonplaceholder.typicode.com/posts', function(err, reqres, body) {
+    
+    let json = JSON.parse(body);
+    console.log(json[0]);
+    res.render('posts.ejs', {posts: json});
+  });
+});
 
 // Show the search form
 app.get('/search', (req, res) => {
@@ -25,4 +33,22 @@ app.get('/search', (req, res) => {
 });
 
 // Find all comments for post
-app.post('/search', (req, res) => {});
+app.post('/search', (req, res) => {
+  request('http://jsonplaceholder.typicode.com/posts', function(err, reqres, body) {
+    let json = JSON.parse(body);
+    let postId = "";
+    for (let x = 0; x < json.length; x++){
+      if (json[x].body.name == json[x].name) postId = json[x].id;
+    }
+    console.log(postId);
+    request('http://jsonplaceholder.typicode.com/comments', function(newerr, newreqres, newbody) {
+      let newjson = JSON.parse(newbody);
+      let response = [];
+      for (let x = 0; x < newjson.length; x++){
+        if (newjson[x].postId == postId) response.push(newjson[x]);
+      }
+      console.log(response);
+      res.render('search_result.ejs', {comments: response});
+    });
+  });
+});
